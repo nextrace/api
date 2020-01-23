fa1f4b71311173ed8a7b38f65a15aa643a343f72
Andrei <andrei@laye.red>
2020-01-23 10:31:14 +0100

    Updated - Make analytics endpoints GET, to bypass OPTIONS

--- src/controllers/analytics.js
+++ src/controllers/analytics.js
@@ -2,6 +2,16 @@ const router = require('express').Router()
 const { PubSub } = require('@google-cloud/pubsub')
 
 // record impressions for Events
+router.get('/event-impressions', async (req, res) => {
+	const pubsub = new PubSub()
+
+	const dataBuffer = Buffer.from(JSON.stringify({ events: req.body }))
+	pubsub.topic('event-impressions').publish(dataBuffer)
+
+	res.json(true)
+})
+
+// record impressions for Events
 router.post('/event-impressions', async (req, res) => {
 	const pubsub = new PubSub()
 
