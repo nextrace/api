bc38aec39e3c6c61cfff4f0eb9da7df33e130238
Andrei <andrei@laye.red>
2020-01-28 22:03:55 +0100

    Updated - knex db pooling options

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,17 @@
 const knex = require('knex')
 const { IncomingWebhook } = require('@slack/webhook')
 
-exports.knex = knex(process.env.DATABASE_URL)
+exports.knex = knex({
+    client: 'mysql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+        min: 0,
+        max: 10,
+        afterCreate(conn, done) {
+            console.log('Knex', 'threadId:', conn.threadId, 'uid:', conn.__knexUid)
+            done(false, conn)
+        }
+    },
+})
+
 exports.slack = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL)
