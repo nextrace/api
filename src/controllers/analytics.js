fb40376de67c35cb781341d806d6b300349e6d2d
Andrei <andrei@laye.red>
2020-01-29 11:28:31 +0100

    Updated - GET endpoitn for analytics

--- src/controllers/analytics.js
+++ src/controllers/analytics.js
@@ -3,12 +3,17 @@ const { PubSub } = require('@google-cloud/pubsub')
 
 // record impressions for Events
 router.get('/event-impressions', async (req, res) => {
-	const pubsub = new PubSub()
 
-	const dataBuffer = Buffer.from(JSON.stringify({ events: req.body }))
-	pubsub.topic('event-impressions').publish(dataBuffer)
+	if (req.query.events) {
+		const pubsub = new PubSub()
+		const payload = { events: req.query.events.map(id => parseInt(id, 10)) }
+		const dataBuffer = Buffer.from(JSON.stringify(payload))
 
-	res.json(true)
+		pubsub.topic('event-impressions').publish(dataBuffer)
+		return res.json(true)
+	}
+
+	return res.status(400).json(false)
 })
 
 // record impressions for Events
