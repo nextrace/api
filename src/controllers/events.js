51eab692451f05839c4612dc81c74a3bfb7bfd4b
Andrei <andrei@laye.red>
2020-03-28 13:53:57 +0100

    Updated - total & links for /events endpoint

--- src/controllers/events.js
+++ src/controllers/events.js
@@ -7,7 +7,10 @@ const upload = multer({ dest: '/tmp/' })
 const sharp = require('sharp')
 const moment = require('moment')
 const { knex, categoryFields, raceFields } = require('../utils.js')
+const querystring = require('querystring')
 
+// data cache
+const cacheCategoryIds = {}
 
 // list all events, with search too
 router.get('/', async (req, res) => {
@@ -20,7 +23,7 @@ router.get('/', async (req, res) => {
 		return res.status(401).json('Authentication required')
 	}
 
-	let filters = {
+	const filters = {
 		status:			'public',
 		country:		req.query.country || 'ES',	// only filter avoided to be `all`
 		countyState:	req.query.countyState || 'all',
@@ -31,14 +34,16 @@ router.get('/', async (req, res) => {
 		featured:		req.query.featured === '1',
 	}
 
-	const page = Math.min(Math.max(parseInt(req.query.page) || 1, 1), 10)			// restrict to max page 10 atm
-	const perPage = Math.min(Math.max(parseInt(req.query.perPage) || 50, 5), 100)	// max 100 results
+	const pag = {
+		total:		0,
+		pages:		0,
+		page:		Math.min(Math.max(parseInt(req.query.page) || 1, 1), 10),		// restrict to max page 10 atm
+		perPage:	Math.min(Math.max(parseInt(req.query.perPage) || 50, 5), 100),	// max 100 results
+	}
 
-	const eventsQuery = knex('event').distinct('event.id')
-						.select(['event.name', 'event.slug', 'event.date', 'event.date_end', 'event.links', 'event.location_name', 'event.location_locality', 'event.location_county_state', {location_country_name: 'country.name'}, {location_country_code: 'country.code'}, {location_country_code3: 'country.code3'}])
+	const eventsQuery = knex('event')
 						.innerJoin('country', 'event.location_country_id', 'country.id')
 						.innerJoin('event_category', 'event.id', 'event_category.event_id')
-						.innerJoin('race', 'event.id', 'race.event_id')
 						.where('event.status', filters.status)
 
 	// Country filter
@@ -53,10 +58,13 @@ router.get('/', async (req, res) => {
 
 	// Category filter
 	if (filters.category !== 'all') {
-		let category = await knex('category').where('slug', filters.category).first()
+		if (!cacheCategoryIds[filters.category]) {
+			const category = await knex('category').select('id').where('slug', filters.category).first()
+			cacheCategoryIds[filters.category] = category ? category.id : null
+		}
 
-		if (category) {
-			eventsQuery.andWhere('event_category.category_id', category.id)
+		if (cacheCategoryIds[filters.category]) {
+			eventsQuery.andWhere('event_category.category_id', cacheCategoryIds[filters.category])
 		} else {
 			return res.status(400).json({ message: 'Not a valid category' })
 		}
@@ -65,7 +73,7 @@ router.get('/', async (req, res) => {
 	// Distance filter
 	if (filters.distance !== 'all') {
 		let [min, max] = filters.distance.split(',').map(d => parseInt(d, 10))
-		eventsQuery.andWhere('race.distance', '>=', min || 0).andWhere('race.distance', '<=', (max || 9999) + .5)
+		eventsQuery.andWhere('event.distance_min', '>=', min || 0).andWhere('event.distance_max', '<=', (max || 9999) + .5)
 	}
 
 	// Date filter
@@ -107,10 +115,20 @@ router.get('/', async (req, res) => {
 		eventsQuery.andWhere('event.featured', 1)
 	}
 
+	// Query total number of events
+	const totalQuery = eventsQuery.clone().count({ total: 'event.id' }).first()
+	pag.total = (await totalQuery).total
+	pag.pages = Math.ceil(pag.total / pag.perPage)
+
+	// Select needed fields
+	eventsQuery
+		.distinct('event.id')
+		.select(['event.name', 'event.slug', 'event.date', 'event.date_end', 'event.links', 'event.location_name', 'event.location_locality', 'event.location_county_state', {location_country_name: 'country.name'}, {location_country_code: 'country.code'}, {location_country_code3: 'country.code3'}])
+
 	// Order & pagination
-	eventsQuery.orderBy('event.date', 'ASC').offset((page - 1) * perPage).limit(perPage)
+	eventsQuery.orderBy('event.date', 'ASC').offset((pag.page - 1) * pag.perPage).limit(pag.perPage)
 
-
+	// Get events & related data
 	let events = await eventsQuery
 	const eventIds = events.map(event => event.id)
 	let races = events.length ? await knex('race').select(raceFields).whereIn('event_id', eventIds) : []
@@ -135,9 +153,24 @@ router.get('/', async (req, res) => {
 		return event
 	})
 
-	res.set('Cache-Control', 'public, max-age=7200')
+	// build Links
+	const links = {
+		self:	`/events?${querystring.stringify({ ...req.query, ...{perPage: pag.perPage, page: pag.page} })}`,
+	}
 
-	res.json(events)
+	if (pag.page > 1) {
+		links.prev = `/events?${querystring.stringify({ ...req.query, ...{perPage: pag.perPage, page: pag.page - 1} })}`
+	}
+
+	if (pag.page < pag.pages) {
+		links.next = `/events?${querystring.stringify({ ...req.query, ...{perPage: pag.perPage, page: pag.page + 1} })}`
+		links.last = `/events?${querystring.stringify({ ...req.query, ...{perPage: pag.perPage, page: pag.pages} })}`
+	}
+
+	return res.set({
+		'Cache-Control':	'public, max-age=7200',
+		'X-Total': 			pag.total,
+	}).links(links).json(events)
 })
 
 
