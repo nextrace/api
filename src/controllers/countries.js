3b0ece06d03e063adfee2668302c3df7284478d9
Andrei <andrei@laye.red>
2020-05-29 15:49:46 +0200

    Added - Timezone list for countries

--- src/controllers/countries.js
+++ src/controllers/countries.js
@@ -1,10 +1,19 @@
 const router = require('express').Router()
 const { knex } = require('../utils.js')
 
+const countryFields = ['code', 'code3', 'name', 'continent', 'capital', 'timezones']
+
+const processCountry = country => {
+
+	country.timezones = JSON.parse(country.timezones)
+
+	return country
+}
+
 // list all countries
 router.get('/', async (req, res) => {
-	const fields = ['code', 'code3', 'name', 'continent', 'capital']
-	const countries = knex('country')
+	
+	let countries = knex('country')
 
 	if (req.query.q && req.query.q.length > 1) {
 		countries
@@ -14,21 +23,26 @@ router.get('/', async (req, res) => {
 			.orWhere('capital', 'LIKE', `${req.query.q}%`)
 	}
 
-	res.json(await countries.select(fields))
+	countries = await countries.select(countryFields)
+	countries = countries.map(processCountry)
+
+	res.json(countries)
 })
 
 // get one country
 router.get('/:code', async (req, res) => {
-	const country = await knex('country')
-						.select('code', 'code3', 'name', 'continent', 'capital')
+	let country = await knex('country')
+						.select(countryFields)
 						.where('code', req.params.code).orWhere('code3', req.params.code).orWhere('name', req.params.code)
 						.first()
 
-	if (country) {
-		res.json(country)
-	} else {
+	if (!country) {
 		res.status(404).json({ message: 'Country code not found' })
 	}
+
+	country = processCountry(country)
+
+	return res.json(country)
 })
 
 module.exports = router
