203fd1d8925a291fda2677389b6db29f9612baad
Andrei <andrei@laye.red>
2021-10-24 17:55:56 +0200

    Updated - include photo url for people

--- src/controllers/people.js
+++ src/controllers/people.js
@@ -4,14 +4,15 @@ const { knex } = require('../utils.js')
 
 // Person profile
 router.get('/:id', async (req, res) => {
-	let person = await knex('user').select(['name', 'email', 'handle', 'country_code', 'language', 'meta', 'created_at'])
+	let person = await knex('user').select(['id', 'name', 'email', 'handle', 'country_code', 'language', 'meta', 'created_at'])
 				.where('id', req.params.id).orWhere('handle', req.params.id).first()
 
 	if (!person) {
 		return res.sendStatus(404)
 	}
 
-	person.email_hash = crypto.createHash('md5').update(person.email).digest('hex')
+	const email_hash = crypto.createHash('md5').update(person.email).digest('hex')
+	person.photo = person.picture_url || `https://www.gravatar.com/avatar/${email_hash}?s=256&d=https://files.layered.market/neutral-2.png`
 	person.meta = JSON.parse(person.meta)
 	person.bio = person.meta.bio || ''
 	person.url = person.meta.url || ''
