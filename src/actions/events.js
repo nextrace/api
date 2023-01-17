83ed56cf3d19222f0a51dbba274cf555c4fdf65d
Andrei <andrei@laye.red>
2023-01-17 16:13:13 +0200

    updated: include status field for events

--- src/actions/events.js
+++ src/actions/events.js
@@ -1,6 +1,6 @@
 const { format } = require('date-fns')
 
-const eventFields = ['event.name', 'event.slug', 'event.date', 'event.date_end', 'event.timezone', 'event.links', 'event.location_name', 'event.location_street', 'event.location_locality', 'event.location_county_state', {location_country: 'country.code'}, {location_country_name: 'country.name'}, 'event.location_lat_lng', 'event.category_tags']
+const eventFields = ['event.name', 'event.status', 'event.slug', 'event.date', 'event.date_end', 'event.timezone', 'event.links', 'event.location_name', 'event.location_street', 'event.location_locality', 'event.location_county_state', {location_country: 'country.code'}, {location_country_name: 'country.name'}, 'event.location_lat_lng', 'event.category_tags']
 const raceFields = ['event_id', 'id', 'name', 'category_id', 'category_tag', 'grouping', 'date', 'time_limit', 'distance', {ascent: 'elevation'}, 'max_participants', 'link', 'registration_url']
 
 const processEvent = (event, mode) => {
